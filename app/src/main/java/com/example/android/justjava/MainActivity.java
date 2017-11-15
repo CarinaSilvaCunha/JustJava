94339280fc25b912aacce0e3c2626329a946cd49
Carina Cunha <aniracsilva@gmail.com>
2017-11-15 23:23:06 +0000

    Added buttons + and -

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -27,8 +28,29 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        int quantity = 2;
+        display(quantity);
+        displayPrice(quantity * 5);
     }
+
+    /**
+     * This method is called when the plus button is clicked.
+     */
+
+    public void increment (View view) {
+        int quantity = 3;
+        display(quantity);
+    }
+
+    /**
+     * This method is called when the minus button is clicked.
+     */
+
+    public void decrement (View view) {
+        int quantity = 1;
+        display(quantity);
+    }
+
 
     /**
      * This method displays the given quantity value on the screen.
@@ -37,4 +59,14 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+
 }
