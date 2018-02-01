ef90ab511ba17fb1bb60c8a6fb4574f13dffc88d
CarinaSilvaCunha <aniracsilva@gmail.com>
2018-02-01 00:09:47 +0000

    Added EditText and ternary expression to booleans

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,11 +7,15 @@
  */
 
 package com.example.android.justjava;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -27,9 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
 
     /**
      * This method is called when the plus button is clicked.
@@ -48,29 +50,36 @@ public class MainActivity extends AppCompatActivity {
         CheckBox checkWhippedCream = findViewById(R.id.check_whipped_cream);
         // define checkbox has checkWhippedCream, and connect it to the id given on the XML (check_whipped_cream)
         boolean hasWhippedCream = checkWhippedCream.isChecked();
+        // NoteToSelf: Could use have used: ** String hasWhippedCreamResult = hasWhippedCream ? "Yes" : "No"; ** and passed a string with the variable hasWhippedCreamResult already with the Yes No Version
+
         //define the variable @hasWhippedCream that is updated with the state of the checkWhippedCream CheckBox
         //call displayMessage and pass along the state of the variable it needs to build the message
         //Do the same for CheckBox Chocolate
         CheckBox checkChocolate = findViewById(R.id.check_chocolate);
         boolean hasChocolate = checkChocolate.isChecked();
+        // Find Name entered in EditText to pass on the Order as a String
+        EditText editTextName = findViewById(R.id.name_text_view);
+        String userName = editTextName.getText().toString();
+
         // Display message when tapping on Submit Order button, 2 parameters need to be passed: hasWhippedCream and hasChocolate (boolean)
-        displayMessage(createOrderSummary(hasWhippedCream, hasChocolate));
+        displayMessage(createOrderSummary(hasWhippedCream, hasChocolate, userName));
     }
+
 
     /**
      * Create a summary of the order and add it as a text message in the
      */
     // hasWhippedCream is the variable that is updated with the state of the CheckBox with the id (check_whipped_cream)
-    private String createOrderSummary(boolean hasWhippedCream, boolean hasChocolate) {
-        String userName = "Tiago";
+    private String createOrderSummary(boolean hasWhippedCream, boolean hasChocolate, String userName) {
         int price = calculatePrice();
         return  "Name: " + userName + "\n" +
-                "Add whipped cream? " + hasWhippedCream + "\n" +
-                "Add Chocolate? " + hasChocolate + "\n" +
+                "Add Whipped cream? " + (hasWhippedCream ? "Yes" : "No") + "\n" +
+                "Add Chocolate? " + (hasChocolate ? "Yes" : "No") + "\n" +
                 "Quantity: " + quantity + "\n" +
                 "Total: " + price + "€" + "\n" +
                 "Thank You!";
     }
+
 
     /**
      * Calculates the price of the order.
@@ -109,14 +118,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-
-    /**
-     * This method is called to check if we added whipped cream.
-     */
-
-
-
 }
 
 
